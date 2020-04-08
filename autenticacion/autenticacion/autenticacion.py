ad9b409fe7d9a326b507e73dcf6c7940219f4604
Diego padilla <dapadillar@unal.edu.co>
2020-04-08 11:51:35 -0500

    connection to mongo

--- autenticacion/autenticacion/autenticacion.py
+++ autenticacion/autenticacion/autenticacion.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import configparser
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://192.168.0.16:17071/admin"
-mongo = PyMongo(app)
+
+
 
 
 @app.route('/helloworld')
@@ -12,8 +14,23 @@ def hello_world():
 
 @app.route('/hellomongo')
 def hello_mongo():
+
     online_users = mongo.db.users.find({"mongoadmin": True})
     return 'Conectado a mongo'
 
+@app.route('/authentication/<user>/<password>')
+def authentication(user,password):
+    mongo.db.usersdb.find_one_or_404({"user": user})
+    return user+password
+
+
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('../config/configAutentication.ini')
+    app.config["MONGO_URI"] =config['mongoApp']['Uri']+config['mongoApp']['Database']
+    app.config["MONGO_URI"]="mongodb://mongoadmin:secret@192.168.0.16:27018/admin"
+    mongo = PyMongo(app)
+    #cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(app, resources={"*": {"origins": "*"}})
+
     app.run()
