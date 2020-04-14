12d3ad119f51ec25fad43e896ab130f5bf753dc5
juan_lopera <Casa0919>
2020-04-13 20:58:17 -0400

    version inicial

---
+++ filesystem/FileManager/FileManager.py
@@ -0,0 +1,19 @@
+import os, configparser
+from flask import Blueprint, request, jsonify
+from werkzeug.utils import secure_filename
+
+
+fileManager_BP = Blueprint("FileManager", __name__)
+uploadFolder = "/home/osboxes/files/"
+
+
+@fileManager_BP.route('/multipart-upload', methods=['POST'])
+def upload():
+    config = configparser.ConfigParser()
+    config.read("./FileManager/config/FileManager.ini")
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(config["TENANT1"]["file_dir"], filename))
+    resp = jsonify({'message': 'File successfully uploaded'})
+    resp.status_code = 201
+    return resp
