c492ae97bda47af5fc5f0cc488359205ae06e5eb
Diego padilla <dapadillar@unal.edu.co>
2020-04-15 20:08:51 -0500

    authentication with Kong

--- autenticacion/services/Services.py
+++ autenticacion/services/Services.py
@@ -10,12 +10,15 @@ from flask import jsonify
 import datetime
 import uuid
 from bson.json_util import dumps, loads
+import configparser
+import requests
 
 app1 = Blueprint('app11', __name__)
 
 @app1.route('/helloworld')
 def hello_world():
     return 'Hello World!'
+
 
 @app1.route('/authentication/validateuser/',methods = ['POST'])
 def validateUser():
@@ -30,9 +33,12 @@ def validateUser():
         return jsonify({"message": "user/password correct","flag":True,"token":token})
     if userRta=='null':
         return jsonify({"message": "user/password incorrect","flag":False,"token":token}),status.HTTP_401_UNAUTHORIZED
+
+
     objectToinsert={"user": user,"token":token,"generatedDate":datetime.datetime.now()}
     Db.insert("token",objectToinsert)
-    return jsonify({"message": "user/password correct","flag":True,"token":token})
+    #return jsonify({"message": "user/password correct","flag":True,"token":token})
+    return registerConsumerGetToken(user)
 
 @app1.route('/authentication/changePassword/',methods = ['POST'])
 def changePassword():
@@ -79,6 +85,12 @@ def validateUserToken():
     tokenRta = validateToken(token)
     return tokenRta
 
+@app1.route('/authentication/refreshtoken/',methods = ['POST'])
+def refreshToken():
+    data = request.get_json()
+    user=data["user"]
+    token=data["refresh_token"]
+    return refreshTokenUser(user,token)
 
 def getTenant(headers):
     tenat=headers.get("Host")
@@ -100,3 +112,63 @@ def validateToken(token):
         return jsonify({"message": "invalid token","flag":False,"token":token}),status.HTTP_401_UNAUTHORIZED
     return jsonify({"message": "valid token","flag":True,"token":token})
 
+
+def registerConsumerGetToken(user):
+    config = configparser.ConfigParser()
+    config.read('../config/configAutentication.ini')
+
+    amPort=config['am']['Port']
+    amHost=config['am']['Host']
+    amProtocol=config['am']['Protocol']
+    sessionTime=config['am']['SessionTime']
+
+    jsonInfo={"username":user,"custom_id":user}
+    resp = requests.post(amProtocol+'://'+amHost+":"+amPort+'/consumers/', json=jsonInfo)
+    if resp.status_code != 201:
+        print ('POST /consumer/ {}'+format(resp.status_code))
+        if resp.status_code == 400 or resp.status_code == 409:
+            resp = requests.get('http://'+amHost+":"+amPort+'/consumers/'+user)
+            consumerId=resp.json()["id"];
+            print('consumer. ID: {}'+consumerId)
+            booleanConsumerExist=True
+    elif resp.status_code != 200 or resp.status_code != 201:
+        consumerId=resp.json()["id"];
+        print('Created consumer. ID: {}'+format(resp.json()))
+
+
+    jsonInfo={"name":"gestor","client_id":user,"client_secret":user,"redirect_uris":["http://192.168.0.16:7001"]}
+    resp = requests.post(amProtocol+'://'+amHost+":"+amPort+'/consumers/'+consumerId+"/oauth2", json=jsonInfo)
+    oauthId=""
+    if resp.status_code != 201:
+        print ('POST /oatuh credential/ {}'+format(resp.status_code))
+        if resp.status_code == 400 or resp.status_code == 409:
+            resp = requests.get('http://'+amHost+":"+amPort+'/consumers/'+consumerId+'/oauth2')
+            print('consumer. ID: {}'+format(resp.json()))
+            oauthId=resp.json()["data"][0]["id"]
+            print('oauth register. ID: {}'+oauthId)
+
+    elif resp.status_code != 200 or resp.status_code != 201:
+        oauthId=resp.json()["id"]
+        print('Created oatuh credential. ID: {}'+format(resp.json()))
+
+    jsonInfo={"credential":{"id":oauthId},"expires_in":int (sessionTime),"scope":"out","authenticated_userid":user}
+    resp = requests.post(amProtocol+'://'+amHost+":"+amPort+'/oauth2_tokens/', json=jsonInfo)
+    print('Created oatuh credential. ID: {}'+format(resp.json()))
+
+    return resp.json()
+
+
+def refreshTokenUser(user,token):
+    config = configparser.ConfigParser()
+    config.read('../config/configAutentication.ini')
+
+    amPort=config['am']['Port']
+    amHost=config['am']['Host']
+    amProtocol=config['am']['Protocol']
+
+
+    resp = requests.get(amProtocol+'://'+amHost+":"+amPort+'/oauth2_tokens/'+token)
+    if resp.status_code == 201 or resp.status_code == 200:
+        requests.delete(amProtocol+'://'+amHost+":"+amPort+'/oauth2_tokens/'+token)
+        return registerConsumerGetToken(user)
+    return resp.json()
