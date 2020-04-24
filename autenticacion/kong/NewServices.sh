0d93b9c0d8adb1cbccb5c0c5f604ebe468f922d8
Diego padilla <dapadillar@unal.edu.co>
2020-04-24 17:00:18 -0500

    authentication with Kong

--- autenticacion/kong/NewServices.sh
+++ autenticacion/kong/NewServices.sh
@@ -1,14 +1,19 @@
 #!bin/bash
 curl -i -X POST \
-  --url http://127.0.0.1:8001/kongkong/registerservice/ \
-  -d {"servicename":   "helloworld22","url":"http://authenticationgestor:5000/helloworld",
+  --url http://127.0.0.1:5000/kongkong/registerserviceoauth/ \
+  -H "Content-Type: application/json" \
+  -d '{"servicename":  "helloworld22","url":"http://authenticationgestor:5000/helloworld",
       "consumerhosts":["192.168.0.16"],
       "gatewaypaths":["/helloworld22"],
-      "methods":["GET"],"scopes":["FN"]}
+      "methods":["GET"],"scopes":["FN"]
+      }'
 
 curl -i -X POST \
-  --url http://127.0.0.1:8001/kongkong/registerservice/ \
-  -d {"servicename":   "validateuser","url":"http://authenticationgestor:5000/authentication/validateuser/",
+  --url http://127.0.0.1:5000/kongkong/registerservice/ \
+  -H "Content-Type: application/json" \
+  -d '{"servicename": "validateuser",
+      "url":"http://authenticationgestor:5000/authentication/validateuser/",
       "consumerhosts":["192.168.0.16"],
       "gatewaypaths":["/authentication/validateuser/"],
-      "methods":["POST"],"scopes":["FN"]}
+      "methods":["POST"],"scopes":["FN"]
+      }'
--- autenticacion/services/Kong.py
+++ autenticacion/services/Kong.py
@@ -17,6 +17,50 @@ sys.path.append('../')
 
 app2 = Blueprint('app12', __name__)
 
+@app2.route('/kongkong/registerserviceoauth/',methods = ['POST'])
+def registerserviceoauth():
+    config = configparser.ConfigParser()
+    config.read('../config/configAutentication.ini')
+
+    amPort=config['am']['Port']
+    amHost=config['am']['Host']
+    amProtocol=config['am']['Protocol']
+
+    data = request.get_json()
+    servicename=data["servicename"]
+    url=data["url"]
+    consumerhosts=data["consumerhosts"]
+    gatewaypaths=data["gatewaypaths"]
+    methods=data["methods"]
+    scope=data["scopes"]
+
+    jsondata={"name":servicename, "url":url}
+    resp = requests.post(amProtocol+'://'+amHost+":"+amPort+'/services/',json=jsondata)
+    print('Created service '+format(resp.json()))
+    if resp.status_code == 201 or resp.status_code == 200:
+        service={"id":resp.json()["id"]}
+        jsondata={"hosts":consumerhosts}
+        resp=requests.post(amProtocol+'://'+amHost+":"+amPort+'/services/'+servicename+'/routes',json=jsondata)
+        print('Created service route'+format(resp.json()))
+        if resp.status_code == 201 or resp.status_code == 200:
+            jsondata={"hosts":consumerhosts, "paths":gatewaypaths,"service":service,"methods":methods, "protocols":["http","https"]}
+            resp=requests.post(amProtocol+'://'+amHost+":"+amPort+'/routes/',json=jsondata)
+            print('Created route gateway '+format(resp.json()))
+            if resp.status_code == 201 or resp.status_code == 200:
+                jsondata={"name":"oauth2",
+                          "config":{"enable_authorization_code":True,
+                                    "global_credentials":True,
+                                    "enable_password_grant":True,
+                                    "accept_http_if_already_terminated":False,
+                                    "scopes":scope,
+                                    "mandatory_scope":True
+                                    }
+                          }
+                resp=requests.post(amProtocol+'://'+amHost+":"+amPort+'/services/'+servicename+"/plugins",json=jsondata)
+                print('Created oauth seriuce '+format(resp.json()))
+
+    return resp.json()
+
 @app2.route('/kongkong/registerservice/',methods = ['POST'])
 def registerservice():
     config = configparser.ConfigParser()
@@ -56,7 +100,7 @@ def registerservice():
                                     "mandatory_scope":True
                                     }
                           }
-                resp=requests.post(amProtocol+'://'+amHost+":"+amPort+'/services/'+servicename+"/plugins",json=jsondata)
+                #resp=requests.post(amProtocol+'://'+amHost+":"+amPort+'/services/'+servicename+"/plugins",json=jsondata)
                 print('Created oauth seriuce '+format(resp.json()))
 
     return resp.json()
