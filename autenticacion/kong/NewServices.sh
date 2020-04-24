ebecba2314943ed58b63e97f470a5a18f6e11d66
Diego padilla <dapadillar@unal.edu.co>
2020-04-24 15:44:05 -0500

    authentication with Kong

--- autenticacion/kong/NewServices.sh
+++ autenticacion/kong/NewServices.sh
@@ -1,7 +1,14 @@
 #!bin/bash
 curl -i -X POST \
-  --url http://127.0.0.1:5000/kongkong/registerservice/ \
+  --url http://127.0.0.1:8001/kongkong/registerservice/ \
   -d {"servicename":   "helloworld22","url":"http://authenticationgestor:5000/helloworld",
       "consumerhosts":["192.168.0.16"],
       "gatewaypaths":["/helloworld22"],
       "methods":["GET"],"scopes":["FN"]}
+
+curl -i -X POST \
+  --url http://127.0.0.1:8001/kongkong/registerservice/ \
+  -d {"servicename":   "validateuser","url":"http://authenticationgestor:5000/authentication/validateuser/",
+      "consumerhosts":["192.168.0.16"],
+      "gatewaypaths":["/authentication/validateuser/"],
+      "methods":["POST"],"scopes":["FN"]}
