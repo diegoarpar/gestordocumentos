a34064432aa4131031e3addc8274892da67d8f98
Diego padilla <dapadillar@unal.edu.co>
2020-05-06 15:22:39 -0500

    primera versión roles de usuario

--- frontend/sede-electronica/src/services/userServices.js
+++ frontend/sede-electronica/src/services/userServices.js
@@ -5,13 +5,14 @@ import SessionCookie from '../utils/session';
 import axios from "axios";
 
 
-
+var hostServices="http://192.168.0.16:8000";
+hostServices="http://localhost:5000";
 function  UsersServices  () {
     
    
 }
 async function  LogIn(data){
-    return axios.post("http://192.168.0.16:8000/authentication/validateuser/",data)
+    return axios.post(hostServices+"/authentication/validateuser/",data)
     .then(data =>{
         return data.data
     })
@@ -19,35 +20,69 @@ async function  LogIn(data){
 async function  GetData(){
     var headers={};
     headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
-    return axios.get("http://localhost:5000/authentication/users/",{headers:headers})
+    headers.Tenant="Tenant de Diego";
+    return axios.get(hostServices+"/authentication/users/",{headers:headers})
     .then(data =>{
         return data.data
     })
 }
 
-async function  CreateUser(data, headers){
-    if(!headers)headers={'Content-Type': 'application/json'}
+async function  CreateUser(data){
+    var headers={'Content-Type': 'application/json'}
     headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
-    return axios.post("http://localhost:5000/authentication/createuser/",data,{headers:headers})
+    return axios.post(hostServices+"/authentication/createuser/",data,{headers:headers})
     .then(data =>{
         return data.data
     })
 }
-async function  UpdateUser(data, headers){
-    if(!headers)headers={'Content-Type': 'application/json'}
+async function  UpdateUser(data){
+    var headers={'Content-Type': 'application/json'}
     headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
-    return axios.post("http://localhost:5000/authentication/createuser/",data,{headers:headers})
+    return axios.post(hostServices+"/authentication/updateUser/",data,{headers:headers})
     .then(data =>{
         return data.data
     })
 }
-async function  ChangePassword(data, headers){
-    if(!headers)headers={'Content-Type': 'application/json'}
+async function  ChangePassword(data){
+    var headers={'Content-Type': 'application/json'}
     headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
-    return axios.post("http://localhost:5000/authentication/changePassword/",data,{headers:headers})
+    return axios.post(hostServices+"/authentication/changePassword/",data,{headers:headers})
     .then(data =>{
         return data.data
     })
 }
 
-export default  {GetData,CreateUser,ChangePassword, UpdateUser,LogIn,UsersServices};
+async function GetRoles(data){
+    var headers={'Content-Type': 'application/json'}
+    headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
+    return axios.post(hostServices+"/authentication/roles/",data,{headers:headers})
+    .then(data =>{
+        return data.data
+    })
+}
+async function  SaveRoles(data){
+    var headers={'Content-Type': 'application/json'}
+    headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
+    return axios.put(hostServices+"/authentication/roles/",data,{headers:headers})
+    .then(data =>{
+        return data.data
+    })
+}
+async function  DeleteRoles(data){
+    var headers={'Content-Type': 'application/json'}
+    headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
+    return axios.delete(hostServices+"/authentication/roles/",data,{headers:headers})
+    .then(data =>{
+        return data.data
+    })
+}
+export default  {GetData,
+    CreateUser,
+    ChangePassword,
+    UpdateUser,
+    LogIn,
+    GetRoles,
+    SaveRoles,
+    UsersServices,
+    DeleteRoles
+};
