91b60852257d3bcccad3c53a2b82c2a9a2d8d8e8
Diego padilla <dapadillar@unal.edu.co>
2020-05-14 09:09:10 -0500

    listar tareas pendientes bpm v1

---
+++ frontend/portal-funcionario/src/services/processTaskServices.js
@@ -0,0 +1,23 @@
+import SessionCookie from '../utils/session';
+import axios from "axios";
+
+
+var hostServices="http://192.168.0.16:8000";
+hostServices="http://localhost:2001";
+function  ProcessTaskServices  () { 
+}
+
+async function GetTask(data){
+    var headers={'Content-Type': 'application/json'}
+    headers.Authentication="Bearer "+SessionCookie.GetSessionCookie().access_token;
+    headers.Tenant=SessionCookie.GetSessionCookieTenant().tenant;
+    return axios.post(hostServices+"/processTask/getTask",data,{headers:headers})
+    .then(data =>{
+        return data.data
+    })
+}
+
+export default  {
+    ProcessTaskServices,
+    GetTask
+};
--- frontend/portal-funcionario/src/views/ventanilla/ventanilla.js
+++ frontend/portal-funcionario/src/views/ventanilla/ventanilla.js
@@ -1,11 +1,13 @@
 import React,{ useState,useEffect } from 'react';
 import MenuProcess from './menuProcess'
+import TaskInformation from './taskInformation'
 
 const VentanillaRadicacion=(props)=>{
 
     return (<div>
             Aquí está la VentanillaRadicacion
             <MenuProcess></MenuProcess>
+            <TaskInformation></TaskInformation>
 
     </div>);
 
