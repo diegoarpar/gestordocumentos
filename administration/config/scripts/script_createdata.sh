97e3b6b64b6c3a49e2a9fdcc8867da8666dd9483
Diego padilla <dapadillar@unal.edu.co>
2020-05-24 17:16:23 -0500

    request number script

---
+++ administration/config/scripts/script_createdata.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+curl -i -X POST \
+  --url http://127.0.0.1:5001/administration/parametricvalue/ \
+  -H "Content-Type: application/json" \
+  -d '{"description":  "AAAAMMDDHHMMSSmmm",
+      "name": "%Y%m%d%H%M%S%f",
+      "type": "REQUEST_NUMBER_PATTERN",
+      "typeDescription": "Patrón radicado",
+      "value": "%Y%m%d%H%M%S%f"
+      }'
+
+curl -i -X POST \
+  --url http://127.0.0.1:5001/administration/parametricvalue/ \
+  -H "Content-Type: application/json" \
+  -d '{"description":  "AAAA/MM/DD/HHMMSSmmm",
+      "name": "%Y/%m/%d %H%M%S%f",
+      "type": "REQUEST_NUMBER_PATTERN",
+      "typeDescription": "Patrón radicado",
+      "value": "%Y%m%d%H%M%S%f"
+      }'
+
+curl -i -X POST \
+  --url http://127.0.0.1:5001/administration/parametricvalue/ \
+  -H "Content-Type: application/json" \
+  -d '{"description":  "DD/MM/AAAA/HHMMSSmmm",
+      "name": "%d/%m/%Y %H%M%S%f",
+      "type": "REQUEST_NUMBER_PATTERN",
+      "typeDescription": "Patrón radicado",
+      "value": "%d%m%Y%H%M%S%f"
+      }'
+
+
+curl -i -X POST \
+  --url http://127.0.0.1:5001/administration/parametricvalue/ \
+  -H "Content-Type: application/json" \
+  -d '{"description":  "DD/MM/AAAA/HHMMSSmmm",
+      "name": "%d%m%Y %H%M%S%f",
+      "type": "REQUEST_NUMBER_PATTERN",
+      "typeDescription": "Patrón radicado",
+      "value": "%d%m%Y%H%M%S%f"
+      }'
